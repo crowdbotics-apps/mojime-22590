064f78dbbd08064d527bbd1be46a6b4a47118cfc
Crowdbotics <team@crowdbotics.com>
2020-11-13 20:05:14 +0000

    remove package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -3,7 +3,6 @@ export const installed_blueprints = [
 { name: 'Maps172589', human_name: 'Maps', access_route: 'Maps172589', icon: 'map'},
 { name: 'Additem172588', human_name: 'AddItem', access_route: 'Additem172588'},
 { name: 'Maps172584', human_name: 'Maps', access_route: 'Maps172584', icon: 'map'},
-{ name: 'UserProfile172580', human_name: 'User Profile', access_route: 'UserProfile172580'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import Maps172589Navigator from '../features/Maps172589/navigator';
 import Additem172588Navigator from '../features/Additem172588/navigator';
 import Maps172584Navigator from '../features/Maps172584/navigator';
-import UserProfile172580Navigator from '../features/UserProfile172580/navigator';
 
 /**
  * new navigators can be imported here
@@ -20,7 +19,6 @@ const AppNavigator = {
 Maps172589: { screen: Maps172589Navigator },
 Additem172588: { screen: Additem172588Navigator },
 Maps172584: { screen: Maps172584Navigator },
-UserProfile172580: { screen: UserProfile172580Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
