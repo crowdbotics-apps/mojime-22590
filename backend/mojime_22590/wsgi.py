1c65b7330c2a678b32976dbb0b44bb5989a42f88
Crowdbotics <team@crowdbotics.com>
2020-11-13 17:32:56 +0000

    Update models

--- backend/mojime_22590/wsgi.py
+++ backend/mojime_22590/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mojime_22590.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mojime_22590.settings")
 
 application = get_wsgi_application()
