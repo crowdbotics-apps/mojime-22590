3aba1f8d02f108047c783bb9e7d5344ee2cf48ad
Crowdbotics <team@crowdbotics.com>
2020-11-13 16:53:36 +0000

    add package blueprint-react-native-sign-in-1-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn11172557', human_name: 'Sign In 1 1', access_route: 'SignIn11172557'},
 { name: 'Settings3172556', human_name: 'Settings 3', access_route: 'Settings3172556'},
 { name: 'Dashboard15172554', human_name: 'Dashboard 1 5', access_route: 'Dashboard15172554'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn11172557Navigator from '../features/SignIn11172557/navigator';
 import Settings3172556Navigator from '../features/Settings3172556/navigator';
 import Dashboard15172554Navigator from '../features/Dashboard15172554/navigator';
 
@@ -15,6 +16,7 @@ import Dashboard15172554Navigator from '../features/Dashboard15172554/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn11172557: { screen: SignIn11172557Navigator },
 Settings3172556: { screen: Settings3172556Navigator },
 Dashboard15172554: { screen: Dashboard15172554Navigator },
 
